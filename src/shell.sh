311ed3635ea0840372539f8e69ff5a57c078df76
GabrielMM182 <gmm_morais@hotmail.com>
2021-10-05 10:41:13 -0300

    qr code

---
+++ src/shell.sh
@@ -0,0 +1,9 @@
+ionic cordova plugin add cordova-plugin-qrscanner
+
+npm install @ionic-native/qr-scanner
+
+modificando o icon e o splash 
+
+- icon é oq vai aparecer como icone do app
+
+- splash é oq vai aparecer como "loading"
